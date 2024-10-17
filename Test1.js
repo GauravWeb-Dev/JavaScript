f643b033273a68e0d61ec408bfbcc9f55441ce80
GauravWeb-Dev <gauravsardar27@gmail.com>
2024-10-17 01:09:39 -0700

    add 1,2,3,4,5 file

---
+++ Test1.js
@@ -0,0 +1,15 @@
+console.log("Hello World");
+
+ var x = 100;
+
+if(x > 10){
+    var a = 99;
+    console.log("x is greater than 10");
+    console.log("x is greater than 10ss");
+    console.log("x is greater than 10");
+    console.log("x is greater than 10ss");
+
+}
+
+
+
---
+++ Test2.js
@@ -0,0 +1,94 @@
+// var x = 100;
+// let y = 10;
+// const k = true;
+
+
+/*
+ long comment 
+
+*/
+
+//DataTypes
+/*
+1.Number
+2.String
+3.Boolean
+4.undefined
+5.Null
+6.BigInt
+7.Object
+8.Symbol
+*/
+
+
+//  let x = 100;
+
+//  let y = 12.5;
+
+//  console.log(x);
+
+//  console.log(typeof(x));
+
+//  console.log(typeof(y));
+
+
+// let firstName = `Akshay`;
+// let lastName = "Kumar";
+// let batch = 'class B';
+
+// console.log(firstName);
+// console.log(typeof(firstName));
+
+
+
+// let b1 = true;
+// let b2 = false;
+
+// console.log(b1);
+// console.log(typeof b1);
+
+
+// let x ;
+
+// console.log(x);
+// console.log(typeof x);
+
+// let l = null;
+// console.log(l);
+// console.log(typeof l);
+
+
+// const key = Symbol("12345678");
+
+// console.log(key);
+
+
+// let v = 9999999999999999999n;
+
+// console.log(typeof v);
+
+
+let student={
+    id : 1,
+   name : "salman",
+
+}
+
+console.log(student);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
